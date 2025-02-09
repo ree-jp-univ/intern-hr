a62c664b9ffa289b13257f8db56617b27a838ccd
ree-jp <mail@ree-jp.net>
2025-02-09 08:32:31 +0000

    ✨ 編集ページでデータがあるときは読み込めるように デコードしなきゃいけないの知らなくて沼った

--- fuel/app/views/edit/editor.jsx
+++ fuel/app/views/edit/editor.jsx
@@ -7,10 +7,25 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { locales } from "@blocknote/core";
 import "./editor.css";
 
+// HTMLエンティティをデコードする関数
+function decodeHtmlEntities(str) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = str;
+    return txt.value;
+}
+
 export default function App() {
     const memo = window.__INITIAL_DATA__ || [];
 
+    let initialContent = [{
+        type: "paragraph",
+        content: "",
+    }];
+    const decoded = decodeHtmlEntities(memo['content_json']);
+    initialContent = JSON.parse(decoded);
+
     const editor = useCreateBlockNote({
+        initialContent: initialContent,
         dictionary: locales.ja,
     });
 
@@ -29,20 +44,24 @@ export default function App() {
                 <span class="content-title">{memo['title']}</span>
                 <div class="header-btns">
                     <button class="btn-primary header-btn">公開する</button>
-                    <button class="btn-primary header-btn" onClick={() => {
+                    <button class="btn-primary header-btn" onClick={async () => {
                         const json_data = editor.document;
-                        const html_data = editor.blocksToFullHTML(editor.document);
-                        // fetch('/edit/update', {
-                        //     method: 'POST',
-                        //     headers: {
-                        //         'Content-Type': 'application/json',
-                        //     },
-                        //     body: JSON.stringify({
-                        //         memo_id: memo['memo_id'],
-                        //         json: json_data,
-                        //         html: html_data,
-                        //     }),
-                        // })
+                        // const html_data = editor.blocksToFullHTML(editor.document);
+                        const params = new URLSearchParams();
+                        params.append('memo_id', memo['memo_id']);
+                        params.append('content_json', JSON.stringify(json_data));
+                        const response = await fetch('/api/memo/update', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                            },
+                            body: params.toString(),
+                        });
+                    
+                        if (response.ok) {
+                            // 更新成功後にホーム画面にリダイレクト
+                            window.location.href = '/';
+                        }
                     }}>保存する</button>
                 </div>
             </div>
